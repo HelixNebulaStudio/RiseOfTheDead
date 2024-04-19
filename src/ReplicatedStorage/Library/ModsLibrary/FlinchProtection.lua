e62d82c6eeb3244bad28a6cfcf8f642b600dca4e
Khronos <38783502+MXKhronos@users.noreply.github.com>
2024-04-19 19:20:23 +0800

    Fixed FlinchProtection stacking.

--- src/ReplicatedStorage/Library/ModsLibrary/FlinchProtection.lua
+++ src/ReplicatedStorage/Library/ModsLibrary/FlinchProtection.lua
@@ -14,23 +14,10 @@ function Mod.Activate(packet)
 		value = value + tweakVal;
 	end
 	
-	if module.FlinchMod and value < module.FlinchMod then return end;
+	if module.FlinchMod then return end;
 	module.FlinchMod = value;
 
 	module.FlinchProtection = module.FlinchProtection + value;
-	
-	--local modStorageItem, module = paramPacket.ModStorageItem, paramPacket.WeaponModule;
-	
-	--local info = ModsLibrary.Get(modStorageItem.ItemId);
-	--local values = modStorageItem.Values;
-	
-	--local level = math.clamp((values["F"] or 0), 0, info.Upgrades[1].MaxLevel);
-	--local add = ModsLibrary.NaturalInterpolate(info.Upgrades[1].BaseValue, info.Upgrades[1].MaxValue, level, info.Upgrades[1].MaxLevel);
-	
-	--if module.FlinchMod and add < module.FlinchMod then return end;
-	--module.FlinchMod = add;
-	
-	--module.FlinchProtection = module.FlinchProtection + add;
 end
 
 return Mod;
