73f54c9eb013f36a9b86b511ca3c478f408e39dc
Khronos <38783502+MXKhronos@users.noreply.github.com>
2024-05-18 22:12:27 +0800

    Fixed ShotSplitter fire rate mixup.

--- src/ReplicatedStorage/Library/ModsLibrary/FireRateRpm.lua
+++ src/ReplicatedStorage/Library/ModsLibrary/FireRateRpm.lua
@@ -14,22 +14,6 @@ function Mod.Activate(packet)
 	end
 
 	module.Properties.Rpm = module.Properties.Rpm + value;
-	
-	
-	--local modStorageItem, module = paramPacket.ModStorageItem, paramPacket.WeaponModule;
-	
-	--local info = ModsLibrary.Get(modStorageItem.ItemId);
-	--local values = modStorageItem.Values;
-	
-	--local addRpm = ModsLibrary.NaturalInterpolate(info.Upgrades[1].BaseValue, info.Upgrades[1].MaxValue, values["FR"], info.Upgrades[1].MaxLevel, info.Upgrades[1].Rate);
-
-	--if paramPacket.TweakStat and info.Upgrades[1].TweakBonus then
-	--	local addBonus = info.Upgrades[1].TweakBonus * math.abs(paramPacket.TweakStat/100);
-
-	--	addRpm = addRpm + math.ceil(addBonus);
-	--end
-	
-	--module.Properties.Rpm = module.Properties.Rpm + addRpm;
 end
 
 return Mod;
--- src/ReplicatedStorage/Library/ModsLibrary/ShotSplitter.lua
+++ src/ReplicatedStorage/Library/ModsLibrary/ShotSplitter.lua
@@ -15,7 +15,7 @@ function Mod.Activate(packet)
 	
 
 	local frLayerInfo = ModsLibrary.GetLayer("FR", packet);
-	local frlevel, frValue, frTweak = dmgLayerInfo.Level, dmgLayerInfo.Value, dmgLayerInfo.TweakValue;
+	local frlevel, frValue, frTweak = frLayerInfo.Level, frLayerInfo.Value, frLayerInfo.TweakValue;
 	
 	if frTweak then
 		frValue = frValue + math.floor(frTweak*10)/10;
@@ -30,38 +30,6 @@ function Mod.Activate(packet)
 		module.Configurations.Damage = module.Configurations.Damage * dmgValue;
 	end
 	
-	
-	--local dmglevel = math.clamp((values["D"] or 0), 0, info.Upgrades[1].MaxLevel-paramPacket.TierOffset);
-	--local dmgRatio = ModsLibrary.NaturalInterpolate(info.Upgrades[1].BaseValue, info.Upgrades[1].MaxValue, dmglevel, info.Upgrades[1].MaxLevel);
-
-	--if paramPacket.TweakStat and info.Upgrades[1].TweakBonus then
-	--	local addMulti = info.Upgrades[1].TweakBonus * math.abs(paramPacket.TweakStat/100);
-
-	--	dmgRatio = dmgRatio + addMulti;
-	--end
-	
-	--local baseDamage = module.Configurations.BaseDamage;
-	--local baseFireRate = module.Properties.BaseFireRate;
-	--local baseMultishot = module.Properties.BaseMultishot;
-	
-	---- FireRate
-	--local frlevel = math.clamp((values["FR"] or 0), 0, info.Upgrades[2].MaxLevel-paramPacket.TierOffset);
-	--local addRpm = ModsLibrary.NaturalInterpolate(info.Upgrades[2].BaseValue, info.Upgrades[2].MaxValue, frlevel, info.Upgrades[2].MaxLevel, info.Upgrades[2].Rate);
-
-	--if paramPacket.TweakStat and info.Upgrades[2].TweakBonus then
-	--	local bonusRpm = info.Upgrades[2].TweakBonus * math.abs(paramPacket.TweakStat/100);
-
-	--	addRpm = addRpm + math.floor(bonusRpm*10)/10;
-	--end
-	
-	--module.Properties.Rpm = module.Properties.Rpm + addRpm;
-	
-	--if module.Configurations.ShotSplitter == nil or dmglevel > module.Configurations.ShotSplitter then
-	--	module.Configurations.ShotSplitter = dmglevel;
-	--	module.Properties.Multishot = 2;
-		
-	--	module.Configurations.Damage = module.Configurations.Damage * dmgRatio;
-	--end
 end
 
 return Mod;
