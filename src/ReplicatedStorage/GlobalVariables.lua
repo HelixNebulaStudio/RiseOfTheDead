f84eb132416863723a9c8be828cc49a67c6bf696
Khronos <38783502+MXKhronos@users.noreply.github.com>
2024-07-15 19:33:34 +0800

    2.1.19.0

--- src/ReplicatedStorage/GlobalVariables.lua
+++ src/ReplicatedStorage/GlobalVariables.lua
@@ -1,7 +1,7 @@
 local mxkhronosUserId = 16170943;
 local Vars = {
-	GameVersion="2.1.18";
-	GameBuild="1"; 
+	GameVersion="2.1.19";
+	GameBuild="0"; 
 	
 	EngineMode="RiseOfTheDead";
 	ModeVerLabel = "$UpTime";
