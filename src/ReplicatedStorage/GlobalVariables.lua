dd33d660f0bb61c84b5d2122ae099ab549c87f97
Khronos <38783502+MXKhronos@users.noreply.github.com>
2024-08-16 23:30:16 +0800

    Added customization menu materials: `Leather`, `Plaster`, `Rubber`, `Ceramic`, `Cardboard`, `Carpet`, `RoofShingles`, and `ClayRooftiles`.

--- src/ReplicatedStorage/GlobalVariables.lua
+++ src/ReplicatedStorage/GlobalVariables.lua
@@ -2,7 +2,7 @@ local mxkhronosUserId = 16170943;
 local Vars = {
 	DevVersion="2.2.x";
 	GameVersion="2.1.21";
-	GameBuild="7"; 
+	GameBuild="8"; 
 	
 	EngineMode="RiseOfTheDead";
 	ModeVerLabel = "$UpTime";
